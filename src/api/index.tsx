515035d2584b66aae5b635328d7bb59eafcf7413
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-02-10 12:13:58 -0300

    reset values

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
   withCredentials: true,
   headers: {
-   "ngrok-skip-browser-warning": "69420",
+    "Content-Type": "application/json",
   }
 });
 
