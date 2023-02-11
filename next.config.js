71c3faef9fd089dfb3a136285d1f518b37d3c9ee
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-02-11 14:53:43 -0300

    fix config next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: 'https://api-card-limit-expose-rodolfobelo19.vercel.app/:path*',
+        },
+      ]
+    },
+};
